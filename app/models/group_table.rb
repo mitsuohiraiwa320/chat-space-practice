30ef2de077e663c7336d499f5a3a1202f0469af8
mitsuohiraiwa320 <mitsuo19941206@gmail.com>
2020-02-10 19:49:46 +0900

    修正

--- app/models/group_table.rb
+++
@@ -1,5 +0,0 @@
-class Groups <ActivRecord::Base
-  has_many :groups_users
-  has_many :users, through: groups_users
-  has_many :messages
-end
--- app/models/users_table.rb
+++
@@ -1,5 +0,0 @@
-class Users < ActiveRecord::Base
-  has_many :groups_users
-  has_many :groups, through: groups_users
-  has_many :messages
-end
