4215c6786bd522cb8537d5312b7804c01c402ec9
mitsuohiraiwa320 <mitsuo19941206@gmail.com>
2020-04-11 16:47:58 +0900

    message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message">
+       `<div class="message" data-message-id = ${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="message">
+       `<div class="message" data-message-id = ${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -75,18 +75,18 @@ $(function(){
     .done(function(messages) {
       if (messages.length !== 0){
       var insertHTML = '';
-      $each(messages, function(i,message){
+      $.each(messages, function(i,message){
         insertHTML += buildHTML(message)
       });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.main-chat__messages').append(insertHTML);
+      $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
       }
     })
     .fail(function() {
       alert('error');
     });
+  }
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages,7000);
   }
-}
 });
