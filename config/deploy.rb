41a7cbe6bd89ccefd2a94a79ea467e1505f2b272
mitsuohiraiwa320 <mitsuo19941206@gmail.com>
2020-04-15 13:07:20 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'chat-space-practice'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:mitsuohiraiwa320/chat-space-practice.git'
